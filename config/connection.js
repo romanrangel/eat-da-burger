3abf84fb0cca00b887644d4541b8be843a6aec1f
Roman Rangel <romanrangel106549@gmail.com>
2021-01-27 08:06:01 -0800

    added connection to conncection.js for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
 
-    const connection = mysql.createConnection({
-        host: "localhost",
-        port: process.env.PORT || 3306,
-        user: "root",
-        password: "mazda2020",
-        database: "burger_db"
-    });
+    let connection;
+    if (process.env.JAWSDB_URL) {
+        connection = mysql.createConnection(process.env.JAWSDB_URL)
+    } else {
+        connection = mysql.createConnection({
+            host: "localhost",
+            port: process.env.PORT || 3306,
+            user: "root",
+            password: "mazda2020",
+            database: "burger_db"
+        });
+    }
 
 connection.connect((err) => {
     if (err) {
