85d29a96863d52f152bd41b915ba31638567f8df
Roman Rangel <romanrangel106549@gmail.com>
2021-01-27 07:47:30 -0800

    updated connection to local only

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,5 @@
 const mysql = require("mysql");
 
-if (proccess.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
     const connection = mysql.createConnection({
         host: "localhost",
         port: process.env.PORT || 3306,
@@ -10,8 +7,6 @@ if (proccess.env.JAWSDB_URL) {
         password: "mazda2020",
         database: "burger_db"
     });
-}
-
 
 connection.connect((err) => {
     if (err) {
